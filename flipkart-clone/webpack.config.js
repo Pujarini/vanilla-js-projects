042b55e5f207c51032ce3529f616ddf3f826f19b
Pujarini Jena <pujarinijena@Pujarini-MacBook-Pro.local>
2023-04-09 23:23:21 +0530

    navbar of flipkart-clone created

---
+++ flipkart-clone/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    bundle: path.resolve(__dirname, "/src/script/index.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name][contenthash].js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3001,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      title: "webpack template",
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+  ],
+};
